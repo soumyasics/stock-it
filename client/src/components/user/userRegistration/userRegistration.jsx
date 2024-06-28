84935ef65f05150f07a01af23810b773b8c3bd49
rohinreji <rohinreji65@gmail.com>
2024-06-28 11:32:04 +0530

    ok

--- client/src/components/user/userRegistration/userRegistration.jsx
+++ client/src/components/user/userRegistration/userRegistration.jsx
@@ -1,12 +1,23 @@
 import CommonNavbar from "../../common/commonNavbar";
 import { Footer2 } from "../../common/footer2/footer2";
+import './userRegistration.css'
+import img1 from '../../../assets/images/shieldLogo.png'
+
 
 export const UserRegistration = () => {
   return (
     <div>
       <CommonNavbar />
       <div style={{ minHeight: "500px" }}>
-        <h1> User Registration page</h1>
+        <div className="userRegistration">
+          <div className="userRegistration-main">
+            <h1>User Registration</h1>
+              <div className="userRegistration-logo">
+                <img src={img1} alt="logo" />
+
+              </div>
+          </div>
+        </div>
       </div>
       <Footer2 />
     </div>
