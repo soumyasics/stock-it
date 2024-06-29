2f5810b91cecd06a17a9e4cfa7dc74389345d881
rohinreji <rohinreji65@gmail.com>
2024-06-29 13:46:42 +0530

    companysidebar

--- client/src/components/company/companySidebar/companySidebar.jsx
+++ client/src/components/company/companySidebar/companySidebar.jsx
@@ -5,9 +5,13 @@ import titleImg from '../../../assets/images/Group 398.png'
 import img1 from '../../../assets/images/ipos.png'
 import img2 from '../../../assets/images/divident.svg'
 import img3 from '../../../assets/images/orderIcon.svg'
+import img4 from '../../../assets/images/article.svg'
+import img5 from '../../../assets/images/action.png'
+import img6 from '../../../assets/images/logout.png'
 
 function CompanySidebar() {
     const [show, setShow] = useState(false);
+    const [show1, setShow1] = useState(false);
     return (
         <div>
             <div className='companysidebar-main'>
@@ -33,32 +37,70 @@ function CompanySidebar() {
                     <div className='companysidebar-content-dividend'>
                         <tr>
                             <td><img src={img2} alt="" /></td>
-                           <td><h6>Dividend</h6></td> 
+                            <td><h6>Dividend</h6></td>
                         </tr>
                     </div>
                     <div className='companysidebar-content-receive'>
-                            <tr>
-                                <td><img src={img3} alt="" /></td>
-                                <td> <div className="receiveorder-drop">
-                                <button type="button" onClick={() => setShow(!show)}>
-                                    {" "}
-                                    Companies
-                                </button>
-                                {show && (
-                                    <div className="sidebar-drop-1">
-                                        <ul>
-                                            <li>
-                                               Buy order
-                                            </li>
-                                            <li>
-                                               Sell Order
-                                            </li>
-                                            
-                                        </ul>
-                                    </div>
-                                )}
-                            </div></td>
-                            </tr>
+                        <tr>
+                            <td><img src={img3} alt="" /></td>
+                            <td>
+                                <div className="company-content-drop">
+                                    <button type="button" onClick={() => setShow(!show)}>
+                                        {" "}
+                                        Receive Order
+                                    </button>
+                                    {show && (
+                                        <div className="sidebar-drop">
+                                            <ul>
+                                                <li>
+                                                    Buy order
+                                                </li>
+                                                <li>
+                                                    Sell Order
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    )}
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                    <div className='companysidebar-content-Article'>
+                        <tr>
+                            <td><img src={img4} alt="" /></td>
+                            <td>
+                                <div className="company-content-drop">
+                                    <button type="button" onClick={() => setShow1(!show1)}>
+                                        {" "}
+                                        Articles
+                                    </button>
+                                    {show1 && (
+                                        <div className="sidebar-drop">
+                                            <ul>
+                                                <li>
+                                                    Add Articles
+                                                </li>
+                                                <li>
+                                                    View Articles
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    )}
+                                </div>
+                            </td>
+                        </tr>
+                    </div>
+                    <div className='company-content-complaint'>
+                        <tr>
+                            <td><img src={img5} alt="icon" /></td>
+                            <td><h6>Complaints</h6></td>
+                        </tr>
+                    </div>
+                    <div className='company-content-logout'>
+                        <tr>
+                            <td><img src={img6} alt="icon" /></td>
+                            <td><h6>Logout</h6></td>
+                        </tr>
                     </div>
                 </div>
             </div>
