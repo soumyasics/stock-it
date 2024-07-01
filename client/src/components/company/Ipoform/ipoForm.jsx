56586e04c3ca2e82388750272b4e85cb40edb3e4
Anand R P <anandrp.sics@gmail.com>
2024-07-01 19:03:52 +0530

    ipo form

--- client/src/components/company/Ipoform/ipoForm.jsx
+++ client/src/components/company/Ipoform/ipoForm.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ipoForm.css";
-import AdminNavbar from "../../common/adminNavbar";
+import { toast } from "react-hot-toast";
 
 export const IpoForm = () => {
+  const [totalNoShares, setTotalNoShares] = useState("");
+  const [costPerShare, setCostPerShare] = useState("");
+  const [totalMarketCap, setTotalMarketCap] = useState(0);
+
+  useEffect(() => {
+    const cap = totalNoShares * costPerShare;
+    if (isNaN(cap)) {
+      setTotalMarketCap("");
+      return;
+    }
+    setTotalMarketCap(cap);
+  }, [totalNoShares, costPerShare]);
+
+  const handleTotalNoSharesChange = (e) => {
+    const value = e.target.value;
+    if (value === "") {
+      setTotalNoShares("");
+    } else {
+      const numValue = parseFloat(value);
+      if (numValue >= 0) {
+        setTotalNoShares(numValue);
+      }
+    }
+  };
+
+  const handleCostPerShareChange = (e) => {
+    const value = e.target.value;
+    if (value === "") {
+      setCostPerShare("");
+    } else {
+      const numValue = parseFloat(value);
+      if (numValue >= 0) {
+        setCostPerShare(numValue);
+      }
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!totalNoShares || totalNoShares === 0) {
+      toast.error("Total number of shares cannot be 0");
+    } else if (!costPerShare || costPerShare === 0) {
+      toast.error("Cost per share cannot be 0");
+    } else {
+      sendDataToServer();
+    }
+  };
+  const sendDataToServer = () => {};
   return (
     <div>
       <div style={{ minHeight: "50px" }}></div>
@@ -10,24 +57,47 @@ export const IpoForm = () => {
         <div className="companystock-head">
           <h3>Company Stock Details</h3>
         </div>
-        <div className="companystock-inputs">
+        <form className="companystock-inputs" onSubmit={handleSubmit}>
           <label htmlFor="Totalshares">
             Total Shares <span className="companystock-star">*</span>
           </label>
           <br />
-          <input type="text" placeholder="Total Shares" />
-          <label htmlFor="Totalshares">
-            Cost Per Shares <span className="companystock-star">*</span>
+          <input
+            type="number"
+            value={totalNoShares}
+            className="ps-3"
+            onChange={handleTotalNoSharesChange}
+            placeholder="Total Shares"
+            min="0"
+            max="1000000000"
+          />
+          <label htmlFor="CostPerShares">
+            Cost Per Share <span className="companystock-star">*</span>
           </label>
           <br />
-          <input type="text" placeholder="Cost Per Share" />
-          <label htmlFor="Totalshares">
-            Total Market Captitaion <span className="companystock-star">*</span>
+          <input
+            className="ps-3"
+            type="number"
+            placeholder="Cost Per Share"
+            value={costPerShare}
+            onChange={handleCostPerShareChange}
+            min="0"
+            max="1000000000"
+          />
+          <label htmlFor="TotalMarketCap">
+            Total Market Capitalization{" "}
+            <span className="companystock-star">*</span>
           </label>
           <br />
-          <input type="text" placeholder="Total market capitation" />
-          <button type="sumbit">Submit</button>
-        </div>
+          <input
+            type="text"
+            className="ps-3"
+            placeholder="Total Market Capitalization"
+            value={totalMarketCap}
+            readOnly
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
