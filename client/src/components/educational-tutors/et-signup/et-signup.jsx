2af1144fa705692341c2a3f8b14387e557fb1112
rohinreji <rohinreji65@gmail.com>
2024-07-04 14:23:41 +0530

    et-signup validation

--- client/src/components/educational-tutors/et-signup/et-signup.jsx
+++ client/src/components/educational-tutors/et-signup/et-signup.jsx
@@ -1,13 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import img1 from "../../../assets/images/shieldLogo.png";
 import "./et-signup.css";
 import CommonNavbar from "../../common/commonNavbar";
 import { Footer2 } from "../../common/footer2/footer2";
+import { toast } from "react-hot-toast";
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 function EtSignup() {
+  const [etData, setEtData] = useState({
+    fullName: "",
+    gender: "",
+    qualification: "",
+    specification: "",
+    experience: "",
+    contactNumber: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    photo: null,
+  });
+  const handleChanges = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "contactNumber" && value.length > 10) {
+      return;
+    }
+    if (name === "experience" && value.length > 2) {
+      return;
+    }
+    setEtData({ ...etData, [name]: value });
+  };
+
+  const validateFields = () => {
+    const {
+        fullName,
+        gender,
+        qualification,
+        specification,
+        experience,
+        contactNumber,
+        email,
+        password,
+        confirmPassword,
+        photo,
+      } = etData;
+
+    if (!fullName) {
+      toast.error("Full name is required");
+      return false;
+    }
+    if (!gender) {
+      toast.error("Gender is required");
+      return false;
+    }
+
+    if (!qualification) {
+      toast.error("Qualification is required");
+      return false;
+    }
+    if (!specification) {
+      toast.error("Specification is required");
+      return false;
+    }
+    if (!experience) {
+      toast.error("Experience is required");
+      return false;
+    }
+    if (!contactNumber) {
+      toast.error("Contact Number is required");
+      return false;
+    }
+    if (!email) {
+      toast.error("Eamil is required");
+      return false;
+    }
+    if (!password) {
+      toast.error("Password is required");
+      return false;
+    }
+    if(password.length<8){
+        toast.error("Please Enter minimum 8 characters")
+        return false
+    }
+    if (!confirmPassword) {
+      toast.error("Confirm Password is required");
+      return false;
+    }
+    if(password!==confirmPassword){
+        toast.error("Passwords doesn't matches")
+        return false
+    }
+    if (!photo) {
+      toast.error("Photo is required");
+      return false;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(etData);
+  
+    console.log();
+    if (!validateFields()) {
+      return;
+    }
+  };
+
   return (
     <div>
-      <CommonNavbar/>
+      <CommonNavbar />
       <div style={{ minHeight: "500px" }}>
         <div className="etRegistration">
           <div className="etRegistration-main">
@@ -17,7 +118,7 @@ function EtSignup() {
             <img src={profilePic} alt="Profile" />
           </div> */}
             </div>
-            <form className="etRegistration-content">
+            <form className="etRegistration-content" onSubmit={handleSubmit}>
               <div className="etRegistration-logo">
                 <img src={img1} alt="logo" />
               </div>
@@ -30,7 +131,9 @@ function EtSignup() {
                         type="text"
                         className="form-control"
                         placeholder="Enter Full name"
-                        name="fullname"
+                        name="fullName"
+                        value={etData.fullName}
+                        onChange={handleChanges}
                       />
                     </div>
                     <div className="etRegistration-input-2 col">
@@ -41,13 +144,17 @@ function EtSignup() {
                           className="etRegistration-gender  d-flex"
                           // style={{ height: "30px", width: "100px" }}
                         >
-                          <label className="mt-2" htmlFor="male">Male</label> &nbsp;
+                          <label className="mt-2" htmlFor="male">
+                            Male
+                          </label>{" "}
+                          &nbsp;
                           <input
                             type="radio"
                             className="mt-2"
                             name="gender"
                             id="male"
                             value="male"
+                            onChange={handleChanges}
                           />
                           &nbsp; &nbsp;
                           <label className="ms-3 mt-2 " htmlFor="female">
@@ -60,6 +167,7 @@ function EtSignup() {
                             name="gender"
                             id="female"
                             value="female"
+                            onChange={handleChanges}
                           />
                           &nbsp;
                           <label className="ms-3 mt-2" htmlFor="other">
@@ -67,11 +175,12 @@ function EtSignup() {
                           </label>{" "}
                           &nbsp;
                           <input
-                          className="mt-2"
+                            className="mt-2"
                             type="radio"
                             id="other"
                             name="gender"
                             value="other"
+                            onChange={handleChanges}
                           />
                           &nbsp;
                         </div>
@@ -90,7 +199,9 @@ function EtSignup() {
                         type="text"
                         className="form-control"
                         placeholder="Enter Educational Qualification"
-                        name="edqualification"
+                        name="qualification"
+                        value={etData.qualification}
+                        onChange={handleChanges}
                       />
                     </div>
                   </div>
@@ -101,10 +212,12 @@ function EtSignup() {
                         Area of Specification
                       </label>
                       <input
-                        type="number"
+                        type="text"
                         className="form-control"
                         placeholder="Area of Specification"
                         name="specification"
+                        value={etData.specification}
+                        onChange={handleChanges}
                       />
                     </div>
                     <div className="col mt-4">
@@ -113,7 +226,9 @@ function EtSignup() {
                         type="number"
                         className="form-control"
                         placeholder="Enter Year of Experience"
-                        name="YOE"
+                        name="experience"
+                        value={etData.experience}
+                        onChange={handleChanges}
                       />
                     </div>
                   </div>
@@ -125,7 +240,9 @@ function EtSignup() {
                         type="number"
                         className="form-control"
                         placeholder="Enter Contact Number"
-                        name="contactnumber"
+                        name="contactNumber"
+                        value={etData.contactNumber}
+                        onChange={handleChanges}
                       />
                     </div>
                     <div className="col mt-4">
@@ -134,6 +251,9 @@ function EtSignup() {
                         type="email"
                         className="form-control"
                         placeholder="Enter Email"
+                        name="email"
+                        value={etData.email}
+                        onChange={handleChanges}
                       />
                     </div>
                   </div>
@@ -146,6 +266,8 @@ function EtSignup() {
                         className="form-control"
                         placeholder="Enter Password"
                         name="password"
+                        value={etData.password}
+                        onChange={handleChanges}
                       />
                     </div>
                     <div className="col mt-4">
@@ -154,8 +276,9 @@ function EtSignup() {
                         type="password"
                         className="form-control"
                         placeholder="Re Enter Password"
-                        aria-label="Last name"
                         name="confirmPassword"
+                        value={etData.confirmPassword}
+                        onChange={handleChanges}
                       />
                     </div>
                   </div>
@@ -165,11 +288,15 @@ function EtSignup() {
                       type="file"
                       className="form-control"
                       placeholder="upload"
+                      name="photo"
+                      onChange={(e) => {
+                        setEtData({ ...etData, photo: e.target.files[0] });
+                      }}
                     />
                   </div>
                 </div>
                 <span>
-                  <button  type="submit">Register</button>
+                  <button type="submit">Register</button>
                   <p className="login-link">
                     Already have an account?{" "}
                     <span className="text-primary">Login Now </span>
@@ -180,7 +307,7 @@ function EtSignup() {
           </div>
         </div>
       </div>
-      {<Footer2/>}
+      {<Footer2 />}
     </div>
   );
 }
