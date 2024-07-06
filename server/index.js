0d3ee76dae5ca49f02f642fc2f26d2ae5cf0cea2
rohinreji <rohinreji65@gmail.com>
2024-07-06 12:45:50 +0530

    api forgot

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ const app=express()
 const cors=require('cors')
 const PORT = 4034;
 
+app.use(cors())
 const path=require('path')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(express.static( `${__dirname}/upload`));
 
-app.use(cors())
 const route=require('./routes')
 app.use('/stock_it_api',route)
 
