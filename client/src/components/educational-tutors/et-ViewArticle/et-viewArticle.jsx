655a712ea358fe1a86ef037f336f3904207617ab
rohinreji <rohinreji65@gmail.com>
2024-07-12 16:10:02 +0530

    ViewArticle

--- client/src/components/educational-tutors/et-ViewArticle/et-viewArticle.jsx
+++ client/src/components/educational-tutors/et-ViewArticle/et-viewArticle.jsx
@@ -5,15 +5,18 @@ import { Col, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 function EtviewArticle() {
-  const navigate=useNavigate()
-  const[data,setdata]=useState()
+  const navigate = useNavigate();
+  const [data, setdata] = useState();
   return (
     <div>
       <div className="etViewArticle-main">
         <div className="etViewArticle-head d-flex gap-4">
-          <div className="backToList" onClick={()=>{
-              navigate(-1)
-            }}>
+          <div
+            className="backToList"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
             <IoReturnUpBack />
           </div>
           Article Title
@@ -24,7 +27,7 @@ function EtviewArticle() {
               width="90%"
               height="500px"
               src={
-               "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
               }
               title="YouTube video player"
               frameBorder="0"
@@ -36,42 +39,48 @@ function EtviewArticle() {
             <div className="text-success text-center mt-5 fs-3">
               Description
             </div>
+            <div className="etViewArticle-box2">
+              <table className="mt-5">
+                <tbody>
+                  <tr>
+                    <td>Title</td>
+                    <td>-</td>
+                    <td>fhkuhdhjfajljskflaj</td>
+                  </tr>
+                  <tr>
+                    <td>SubTitle</td>
+                    <td>-</td>
+                    <td>fahja</td>
+                  </tr>
+                  <tr>
+                    <td>Content</td>
+                    <td>-</td>
+                    <td>fqhlfalhfilajfjaopfiaoifhalalfhqilfaklhjjpdio</td>
+                  </tr>
+                  <tr>
+                    <td>Category</td>
+                    <td>-</td>
+                    <td>fuahfllllojk </td>
+                  </tr>
+                  <tr>
+                    <td>Conclusion</td>
+                    <td>-</td>
+                    <td>fquhfaaaaaaaijqogjqgfiyhaflakjifalkkkklof</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
 
-            <table className="text-center mt-5 ms-9" style={{width:"40%"}}>
-              <tbody>
-              <tr>
-                <td>Title</td>
-                <td>-</td>
-                <td>fhkuhdhjfajljskflaj</td>
-              </tr>
-              <tr>
-                <td>SubTitle</td>
-                <td>-</td>
-                <td>fahja</td>
-              </tr>
-              <tr>
-                <td>Content</td>
-                <td>-</td>
-                <td>fqhlfalhfilajfjaopfiaoifhalalfhqilfajjpdio</td>
-              </tr>
-              <tr>
-                <td>Category</td>
-                <td>-</td>
-                <td>fuahfllllojk  </td>
-              </tr>
-              <tr>
-                <td>Conclusion</td>
-                <td>-</td>
-                <td>fquhfaaaaaaaijqogjqgfiyhaflakjifalkkkklof</td>
-              </tr>
-              </tbody>
-            </table>
             <div className="etViewArticle-btn d-flex gap-3">
               <div className="etArticle-editbtn">
-              <button type="button" class="btn btn-outline-primary">Edit</button>
+                <button type="button" class="btn btn-outline-primary">
+                  Edit
+                </button>
               </div>
               <div className="etArticle-deletebtn">
-              <button type="button" class="btn btn-outline-primary">Delete</button>
+                <button type="button" class="btn btn-outline-primary">
+                  Delete
+                </button>
               </div>
             </div>
           </Col>
