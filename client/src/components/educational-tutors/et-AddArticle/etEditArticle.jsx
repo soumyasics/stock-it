8a7f8c5659bb80f4a1a6af356fd2ff4b8ea93933
Anand R P <anandrp.sics@gmail.com>
2024-07-29 00:40:47 +0530

    edit article by tutuor

---
+++ client/src/components/educational-tutors/et-AddArticle/etEditArticle.jsx
@@ -0,0 +1,259 @@
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import axiosMultipartInstance from "../../../apis/axiosMultipartInstance";
+
+export function EditEditArticle({ articleData, editModeOff, articleId }) {
+  const navigate = useNavigate();
+  console.log("arti0 ", articleData);
+  const [coArticle, SetCoArticle] = useState({
+    title: "",
+    subTitle: "",
+    content: "",
+    category: "",
+    coId: "",
+    thumbnail: null,
+    video: null,
+    conclusion: "",
+  });
+
+  console.log("co ar", coArticle)
+  useEffect(() => {
+    SetCoArticle(articleData);
+  }, [articleData]);
+
+  const handleChanges = (e) => {
+    const { name, value } = e.target;
+    SetCoArticle({ ...coArticle, [name]: value });
+  };
+
+  useEffect(() => {
+    const coId = JSON.parse(localStorage.getItem("stock_it_tutorId")) || null;
+    if (coId) {
+      SetCoArticle((prev) => ({
+        ...prev,
+        coId: coId,
+      }));
+    } else {
+      navigate("/companyLogin");
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const {
+      title,
+      subTitle,
+      content,
+      category,
+      coId,
+      thumbnail,
+      video,
+      conclusion,
+    } = coArticle;
+
+    const valitadeFields = () => {
+      if (!title) {
+        toast.error("Enter Title");
+        return false;
+      }
+      if (!subTitle) {
+        toast.error("Enter Subtitle");
+        return false;
+      }
+      if (!content) {
+        toast.error("Enter content");
+        return false;
+      }
+      if (!category) {
+        toast.error("Select category");
+        return false;
+      }
+      if (!thumbnail) {
+        toast.error("Please upload thumbnail");
+        return false;
+      }
+      if (!video) {
+        toast.error("Please upload video");
+        return false;
+      }
+      if (!conclusion) {
+        toast.error("Enter conclusion");
+        return false;
+      }
+      return true;
+    };
+
+    if (!valitadeFields()) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("coId", coId);
+    formData.append("subTitle", subTitle);
+    formData.append("category", category);
+    formData.append("content", content);
+    formData.append("conclusion", conclusion);
+    formData.append("thumbnail", thumbnail);
+    formData.append("video", video);
+
+    sendDataToServer(formData);
+  };
+
+  const sendDataToServer = async (data) => {
+    try {
+      const response = await axiosMultipartInstance.post(
+        `updateArticleById/${articleId}`,
+        data
+      );
+      if (response.status === 200) {
+        toast.success("Article is updated");
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log("Network issue", error);
+    } finally {
+      editModeOff();
+    }
+  };
+
+  return (
+    <div
+      style={{ backgroundColor: "#1f2937", width: "100%", height: "100vh" }}
+      className="d-flex  align-items-center"
+    >
+      <div className="etAddArticle2 pb-5">
+        <div className="etBackBtn ps-4">
+          <div className="etAddArticle-head ms-1 text-center">Article</div>
+        </div>
+        <form onSubmit={handleSubmit} className="etAddArticle-inputs">
+          <div className="row">
+            <div className="col">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Article Title"
+                name="title"
+                value={coArticle.title}
+                onChange={handleChanges}
+              />
+            </div>
+            <div className="col">
+              <label htmlFor="Episode Title">Subtitle </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Article subtitle"
+                name="subTitle"
+                value={coArticle.subTitle}
+                onChange={handleChanges}
+              />
+            </div>
+          </div>
+          <div className="row pt-3">
+            <div className="col">
+              <label htmlFor="title">Content</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Article Content "
+                name="content"
+                value={coArticle.content}
+                onChange={handleChanges}
+              />
+            </div>
+            <div className="col ">
+              <label htmlFor="">Category</label>
+              <select
+                className="form-select-sm "
+                style={{
+                  width: "80%",
+                  height: "30px",
+                  marginLeft: "10%",
+                  borderRadius: "14px",
+                }}
+                aria-label="Default select example"
+                name="category"
+                value={coArticle.category}
+                onChange={handleChanges}
+              >
+                <option value="">Open this select menu</option>
+                <option value="Education">Education</option>
+                <option value="Company Updates">Company Updates</option>
+                <option value="Company News">Company News</option>
+                <option value="Upcoming IPOs">Upcoming IPOs</option>
+                <option value="Stock Analysis">Stock Analysis</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+          </div>
+          <div className="row pt-2">
+            <div className="col">
+              <div className="mb-3">
+                <label htmlFor="formFileSm" className="form-label">
+                  Thumbnail
+                </label>
+                <input
+                  className="form-control form-control-sm"
+                  type="file"
+                  name="thumbnail"
+                  accept="image/*"
+                  onChange={(e) => {
+                    SetCoArticle({
+                      ...coArticle,
+                      thumbnail: e.target.files[0],
+                    });
+                  }}
+                />
+              </div>
+            </div>
+            <div className="col">
+              <div className="mb-3">
+                <label htmlFor="formFileSm" className="form-label">
+                  Video
+                </label>
+                <input
+                  className="form-control form-control-sm"
+                  type="file"
+                  name="video"
+                  accept="video/*"
+                  onChange={(e) => {
+                    SetCoArticle({ ...coArticle, video: e.target.files[0] });
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="etAddArticle-textarea pt-2">
+            Conclusion
+            <br />
+            <input
+              type="textarea"
+              placeholder="Enter conclusion"
+              name="conclusion"
+              value={coArticle.conclusion}
+              onChange={handleChanges}
+            />
+          </div>
+          <div className="etAddArticle-btn mb-5">
+            <button
+              type="button"
+              className="btn btn-outline-danger"
+              onClick={() => {
+                editModeOff();
+              }}
+            >
+              Cancel
+            </button>
+            &nbsp; &nbsp;
+            <button type="submit" className="btn btn-outline-success">
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- client/src/components/educational-tutors/et-ViewArticle/et-viewArticle.jsx
+++ client/src/components/educational-tutors/et-ViewArticle/et-viewArticle.jsx
@@ -6,14 +6,35 @@ import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../../../apis/axiosInstance";
 import { BASE_URL } from "../../../apis/baseUrl";
 import { toast } from "react-hot-toast";
+import { EditEditArticle } from "../et-AddArticle/etEditArticle";
 
 function EtviewArticle() {
   const navigate = useNavigate();
   const [viewArticle, setViewArticle] = useState(null);
   const { id, userType } = useParams();
   const [videoUrl, setVideoUrl] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [pureArticle, setPureArticle] = useState({
+    title: "",
+    subTitle: "",
+    content: "",
+    category: "",
+    thumbnail: null,
+    video: null,
+    conclusion: "",
+  });
   useEffect(() => {
     if (viewArticle) {
+      setPureArticle({
+        title: viewArticle?.title,
+        subTitle: viewArticle?.subTitle,
+        content: viewArticle?.content,
+        category: viewArticle?.category,
+        thumbnail: viewArticle?.thumbnail,
+        video: viewArticle?.video,
+        conclusion: viewArticle?.conclusion,
+      });
+
       let tutorialVideoLink = viewArticle?.video?.filename || null;
       if (tutorialVideoLink) {
         let URL = `${BASE_URL}${tutorialVideoLink}`;
@@ -77,69 +98,98 @@ function EtviewArticle() {
       toast.error("Network issue");
     }
   };
+  const editModeOff = () => {
+    getArticle();
+    setEditMode(false);
+  };
 
   return (
     <div>
-      <div className="etViewArticle-main">
-        <div className="etViewArticle-head d-flex gap-4">
-          <div
-            className="backToList"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            <IoReturnUpBack />
+      {editMode ? (
+        <EditEditArticle
+          articleData={pureArticle}
+          editModeOff={editModeOff}
+          articleId={id}
+        />
+      ) : (
+        <div className="etViewArticle-main">
+          <div className="etViewArticle-head d-flex gap-4">
+            <div
+              className="backToList"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              <IoReturnUpBack />
+            </div>
           </div>
-        </div>
-        <div className="d-flex text-light align-items-center flex-column justify-content-center">
-          <h1 className="text-center text-capitalize ">{viewArticle?.title}</h1>
-          <h6>{viewArticle?.category}</h6>
-        </div>
+          <div className="d-flex text-light align-items-center flex-column justify-content-center">
+            <h1 className="text-center text-capitalize ">
+              {viewArticle?.title}
+            </h1>
+            <h6>{viewArticle?.category}</h6>
+          </div>
 
-        <Row>
-          <Col md={12} className="companyViewArticle-video">
-            {videoUrl && (
-              <iframe
-                width="100%"
-                height="500px"
-                src={videoUrl}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            )}
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} className="companyViewArticle-description">
-            <div className="text-light text-center d-flex justify-content-center mt-5 ">
-              <br />
-              <span className="text-capitalize">{viewArticle?.subTitle}</span>
-            </div>
-            <div className="companyViewArticle-box2 text-light text-justify">
-              <span>{viewArticle?.content}</span>
-              <br />
-              <br />
-              <span>{viewArticle?.conclusion}</span>
-            </div>
-
-            <div className="d-flex mt-5 justify-content-center">
-              {userType === "et" && (
-                <div className="etArticle-deletebtn">
-                  <button
-                    type="button"
-                    onClick={deleteArticle}
-                    className="mx-auto btn btn-outline-danger"
-                  >
-                    Delete
-                  </button>
-                </div>
+          <Row>
+            <Col md={12} className="companyViewArticle-video">
+              {videoUrl && (
+                <iframe
+                  width="100%"
+                  height="500px"
+                  src={videoUrl}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
               )}
-            </div>
-          </Col>
-        </Row>
-      </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12} className="companyViewArticle-description">
+              <div className="text-light text-center d-flex justify-content-center mt-5 ">
+                <br />
+                <span className="text-capitalize">{viewArticle?.subTitle}</span>
+              </div>
+              <div className="companyViewArticle-box2 text-light text-justify">
+                <span>{viewArticle?.content}</span>
+                <br />
+                <br />
+                <span>{viewArticle?.conclusion}</span>
+              </div>
+
+              <div className="d-flex mt-5 justify-content-center">
+                {userType === "et" && (
+                  <>
+                    <div className="etArticle-deletebtn">
+                      <button
+                        type="button"
+                        onClick={() => {
+                          setEditMode(true);
+                        }}
+                        className="mx-auto btn btn-outline-success"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                    &nbsp; &nbsp;
+
+                    <div className="etArticle-deletebtn">
+                      <button
+                        type="button"
+                        onClick={deleteArticle}
+                        className="mx-auto btn btn-outline-danger"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </>
+                )}
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
     </div>
   );
 }
