1fedf4a4eac18c24bc31181befd725182ffe08bd
Anand R P <anandrp.sics@gmail.com>
2024-07-29 01:12:20 +0530

    new build completed all features

--- client/src/apis/axiosInstance.js
+++ client/src/apis/axiosInstance.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-// const axiosInstance = axios.create({
-//   baseURL: "http://hybrid.srishticampus.in/stock_it_api/",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4034/stock_it_api/",
+  baseURL: "http://hybrid.srishticampus.in/stock_it_api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
+// const axiosInstance = axios.create({
+//   baseURL: "http://localhost:4034/stock_it_api/",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 export default axiosInstance;
